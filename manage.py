3ee6ae0ebffe7ad2cea3476910a31d84590a0c51
msadour <sadour.mehdi@gmail.com>
2020-11-28 17:21:01 +0100

    Display starships

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'startships.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'starships.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
